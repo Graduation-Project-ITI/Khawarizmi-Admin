9325d4ea1d910efd505204c0e7255a194a77ee2f
AbdallahNagy <abdallahnagy773@gmail.com>
2023-05-17 17:39:38 +0300

    init components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './Components/cards/cards.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { UsersComponent } from './Components/users/users.component';
+import { CoursesComponent } from './Components/courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    NavbarComponent,
+    UsersComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
